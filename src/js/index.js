559d2eaddcd2867a46b9d32c86e0a0efe4ca4f81
Алексей Н. Мельников <u183@stekspb.ru>
2021-09-28 10:24:23 +0300

    start project Turkish

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+//import styles
+import '@/sass/main';
+import '@/css/style';
+
+//import $ from 'jquery';
+
+//import images
+import adminBG from '@/img/adminBG';
+import supernova from '@/img/supernova';
+import butterfly from '@/img/butterfly';
+import html5 from '@/img/html-5';
+import bear from '@/img/bear';
+import webp from '@/img/webp';
+
+//import js
+import alerts from "./modules/alerts";
+import other from './modules/other';
+
+
+//launch js modules
+alerts();
+other();
